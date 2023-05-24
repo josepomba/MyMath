6b0541c301c5a37ed754ab0b7e270bad66a38b73
josepomba <jpomba@gmail.com>
2023-05-24 09:55:39 +0100

    First Commit

---
+++ MyMath/__init__.py
@@ -0,0 +1,2 @@
+from MyMath.advanced.fib import *
+from MyMath.advanced.sqrt import *
---
+++ MyMath/advanced/fib.py
@@ -0,0 +1,3 @@
+from math import sqrt
+def fibonacci(n):
+    return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
